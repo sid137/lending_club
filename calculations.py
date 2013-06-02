949f6ca1e470726d42d2d70107235295e7155834
Sidney Burks <sid137@gmail.com>
2013-06-02 20:48:40 +0200

    adding virtualenv

--- calculations.py
+++ calculations.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 class Note(object):
-  lending_club_base_interest_rate = 0.0505
+  lending_club_base_interest_rate = 0.0505 # 5.05%
 
   def interest_rate(self):
-      """docstring for interest_rate"""
+      """ 
+      https://www.lendingclub.com/account/rateDetail.action
+      interest_rate = Lending Club Base Rate + Adjustment for Risk & Volatility
+      """
       self.interest_rate = self.lending_club_base_interest_rate + self.risk_and_volatility_adjustment()
 
 
